0211a8b1cceff35bb97123a51c20c46289a5bfec
anantsingh11 <51476103+anantsingh11@users.noreply.github.com>
2019-07-02 13:20:07 -0400

    Update pythonprogram.py

--- pythonprogram.py
+++ pythonprogram.py
@@ -1,1 +1,17 @@
+import socket
 
+s = socket.socket()
+s.bind(('127.0.0.1', 54321))
+s.listen(1)
+while true:
+  client, addr = s.accept()
+  while true:
+    content = client.recv(32)
+    if len(content) ==0:
+      break
+    else:
+      print(content)
+print("client disconnected\n")
+client.close()
+    
+  
