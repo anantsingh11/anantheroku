e5eef460be43f2993daa6b8f4f27de3d2c740621
anantsingh11 <51476103+anantsingh11@users.noreply.github.com>
2019-09-07 08:57:02 -0700

    Update ws_server.py

--- ws_server.py
+++ ws_server.py
@@ -6,6 +6,6 @@ async def response(websocket, path):
 	print("We got the message from the client : " +message)
 	await websocket.send("server received your message")
 
-start_server = websockets.serve(response, 'localhost', 1234)
+start_server = websockets.serve(response, 'ws://anant1.herokuapp.com', 1234)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
